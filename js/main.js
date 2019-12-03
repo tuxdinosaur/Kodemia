f11ac7da9060d954d74fc8bf3b77ddafdb7c7424
tuxdinosaur <ivan.tux@outlook.com>
2019-12-02 22:09:13 -0600

    :octocat: Clase 1, ejercicio 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 console.log('Hello, world')
+
+var amountMXN = 500
+var USDtoMXN = 19.56
+var totalMXNToUSD = amountMXN / USDtoMXN
+
+console.log(amountMXN + 'MXN' + ' es equivalente a ' + totalMXNToUSD + 'USD')
