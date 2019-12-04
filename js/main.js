0114051ad6816924128ad0439686a0d0c936c9fa
tuxdinosaur <ivan.tux@outlook.com>
2019-12-03 19:57:58 -0600

    Ejercicio 1, clase 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log('Hello, world')
+
+var word = "Perros"
+
+var letraUno = word[0]
+var letraDos = word[1]
+var letraTres = word[2]
+var letraCuatro = word[3]
+var letraCinco = word[4]
+
+console.log(letraUno + letraDos + letraTres + letraCuatro + letraCinco)
