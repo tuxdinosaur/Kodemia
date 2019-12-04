71ccc33ade654d91ba7ea29c30220b3215ee96a8
tuxdinosaur <ivan.tux@outlook.com>
2019-12-03 19:20:36 -0600

    :octocat: Tarea 1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
-console.log('Hello, world')
+console.log("Hello, world");
 
-var amountMXN = 500
-var USDtoMXN = 19.56
-var totalMXNToUSD = amountMXN / USDtoMXN
+//Tengo 19 años con 4 meses y 3 dias, ¿cuántos segundos he vivido?
 
-console.log(amountMXN + 'MXN' + ' es equivalente a ' + totalMXNToUSD + 'USD')
+var miEdad = 19;
+
+//DIAS
+var año = 365;
+var mes = 30;
+
+//SEGUNDOS
+var segundosDeUnAño = 2592000 * 12;
+var segundosDeUnMes = 86400 * 30;
+var segundosDeUnDia = 24 * 3600;
+var segundosDeUnaHora = 3600;
+var segundosDeUnMinuto = 60;
+
+console.log("Tengo" + " " + miEdad + " años");
+console.log(
+  "Esquivalente a " +
+    (segundosDeUnAño * 19 + segundosDeUnMes * 4 + segundosDeUnDia * 3) +
+    " segundos vividos :)"
+);
